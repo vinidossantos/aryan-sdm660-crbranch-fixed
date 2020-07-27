8e69b4cf6572be0c4c58c18f87fb4be167dd0535
vinidossantos <loguinsinho@gmail.com>
2020-07-27 13:18:35 -0300

    fingerprint is now coral

--- common_prop.mk
+++ common_prop.mk
@@ -1,5 +1,5 @@
 #Build FP to be picked by both system and vendor
-BUILD_FINGERPRINT := "xiaomi/wayne/wayne:8.1.0/OPM1.171019.011/V9.5.11.0.ODCCNFA:user/release-keys"
+BUILD_FINGERPRINT := "coral-user 10 QQ3A.200605.001 6392402 release-keys"
 
 # ART
 PRODUCT_PROPERTY_OVERRIDES += \
